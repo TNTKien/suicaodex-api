f7fb378e221997a99a5120a1dd29f9fc2783d87f
TNTKien <95180188+TNTKien@users.noreply.github.com>
2025-09-09 11:19:39 +0700

    Add cron_restart configuration to pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  name: "suicaodex_api", // Name of your application
-  script: "src/index.ts", // Entry point of your application
+  name: "suicaodex_api", 
+  script: "src/index.ts", 
+  cron_restart: "0 3 * * *",
   interpreter: "bun", // Bun interpreter
   env: {
     PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
